9276589cacee4fbf94638fede6227509b835c5db
NatanzMeshaal <natanz_saptoaji_33rpl@student.smktelkom-mlg.sch.id>
2024-09-09 14:52:05 +0700

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,10 @@
+package Toko;
+public class Tester {
+    public static void main(String[] args) {
+        //Buat object dari class CD
+        CD cd = new CD();
+        DVD dvd = new DVD();
+        cd.print();
+        dvd.print();
+    }
+}
